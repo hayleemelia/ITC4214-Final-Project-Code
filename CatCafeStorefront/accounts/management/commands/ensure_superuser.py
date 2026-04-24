b14504129456fe135e627e093c82540441bf654d
hayleemelia <h.ayers@acg.edu>
2026-04-24 23:08:55 +0300

    Ensuring a superuser

---
+++ CatCafeStorefront/accounts/management/commands/ensure_superuser.py
@@ -0,0 +1,38 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+
+class Command(BaseCommand):
+    help = "Creates or updates a superuser from environment variables."
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+
+        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+        if not username or not email or not password:
+            self.stdout.write(self.style.WARNING("Superuser environment variables not set. Skipping."))
+            return
+
+        user, created = User.objects.get_or_create(
+            username=username,
+            defaults={
+                "email": email,
+                "is_staff": True,
+                "is_superuser": True,
+            }
+        )
+
+        user.email = email
+        user.is_staff = True
+        user.is_superuser = True
+        user.set_password(password)
+        user.save()
+
+        if created:
+            self.stdout.write(self.style.SUCCESS(f"Superuser '{username}' created."))
+        else:
+            self.stdout.write(self.style.SUCCESS(f"Superuser '{username}' updated."))
